d7f113b2b192c4589940b8d05546967cd7aaca47
sarathmonkr <sarathmonkr1234@gmail.com>
2022-04-28 17:58:19 +0530

    first commit

---
+++ js/calc.js
@@ -0,0 +1,33 @@
+// let val = document.getElementById("number");
+// function calc(a){
+// val.value+=a;
+// }
+// function result(){
+// val.value=eval(val.value);
+// }
+// function clear1(){
+// val.value=val.value.substring(0,val.value.length-1);
+// }
+// function clearScreen(){
+// val.value=" ";
+// }
+
+let val = document.getElementById("result");
+
+function number(dis)
+{	
+	document.getElementById("result").value+=dis;
+}
+
+function equels()
+{
+	val.value=eval(val.value);
+}
+function clear1()
+{
+val.value=val.value.substring(0,val.value.length-1);
+}
+
+function clearScreen(){
+ val.value=" ";
+ }
